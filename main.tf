9955d4cb129fcaee8c49a36fcdc1e51363bf5aab
gayatrishindee <149067079+gayatrishindee@users.noreply.github.com>
2024-03-06 09:32:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,27 @@
 terraform {
+  required_version = "~> 1.1"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
+      version = "~>3.27"
     }
   }
 }
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
 provider "aws" {
-access_key = var.AWS_ACCESS_KEY
-secret_key = var.AWS_SECRET_KEY
-region = "ap-south-1"
-} resource "aws_instance" "myec2" {
-  ami           = "ami-03a6eaae9938c858c"
-  instance_type = "t2.micro"
-  vpc_security_group_ids=[aws_security_group.web-sg.id]
-  key_name="tf-key-pair"  tags={
- Name="web-server"
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
-user_data= <<-EOF
-#!/bin/bash
-yum install httpd -y
-service httpd start
-cd /var/www/html
-touch index.html
-echo "hello from Terraform" > index.html
-EOF
+ 
+resource "aws_instance" "yourec2" {
+  ami                    = "ami-0ba259e664698cbfc"
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "terraform-inst"
+  }
 }
-resource "aws_security_group" "web-sg" {
- name="web-sg"
-ingress {
- from_port=80
- to_port=80
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
-}  ingress {
- from_port=22
- to_port=22
-protocol="tcp"
-cidr_blocks= ["0.0.0.0/0"]
-}
-egress {
- from_port=0
- to_port=0
-protocol="-1"
-cidr_blocks= ["0.0.0.0/0"]
-}
-}
-resource "aws_key_pair" "tf-key-pair-1" {
-key_name = "tf-key-pair"
-public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-algorithm = "RSA"
-rsa_bits  = 4096
-}
-resource "local_file" "tf-key" {
-content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair-1"
+
+variable "access_key" { }
+variable "secret_key" { }
+variable "region_name" {
+ default = "ap-south-1"
 }
